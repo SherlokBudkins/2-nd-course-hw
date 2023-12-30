b8970da542dc9550173ab56e0dcbb1ead00be069
SherlokBudkins <nurochkater20@mail.ru>
2023-12-30 21:45:37 +0300

    HomeWork 4

--- script.js
+++ script.js
@@ -1,63 +1,39 @@
-
-let password = 2000;
-let password1 = prompt('Введите пароль');
-if (password == password1) {
-	alert("Пароль введен верно");
-	
-} else {
-	alert("Пароль введен неправильно");
+let i = 0;
+while (i < 2){
+alert ('Привет');
+i++;
 }
 
-let c = 2;
-if (0 < c && c < 10) {
-	alert('Верно');
-} else {
-	alert('Неверно');
+let n = 1;
+while (n < 6){
+	console.log (n);
+	n++;
+}
+
+for(let k = 7; k < 23; k++){
+	console.log (k);
+}
+
+
+let obj= {
+'Коля': '200',
+'Вася': '300',
+'Петя': '400',
 };
-
-
-let d = 101;
-let e = 11;
-if (d > 100 || e > 100) {
-	alert('Верно');
-} else {
-	alert('Неверно');
-}
-
-let a = '2';
-let b = '3';
-
-alert(Number(a) + Number(b));
-
-let monthNumber = String(prompt('Введите месяц'));
-switch(monthNumber) {
-	case '1':
-	case '2':
-	case '12':		
-		console.log('Зима');
-		break;
-	case '3':
-	case '4':
-	case '5':		
-		console.log('Весна');
-		break;
-	case '6':
-	case '7':
-	case '8':	
-		console.log('Лето');
-		break;
-	case '9':
-	case '10':
-	case '11':		
-		console.log('Осень');
-		break;
-	default:
-		console.log('Такого месяца нет');
-		break;	
+for (let key in obj){
+	alert(`${key} — зарплата ${obj[key]} долларов`);
 }
 
 
+let number = 1000;
+let counter = 0;
+for (;number >= 50; number /= 2){
+	counter++;
+}
+console.log(`Получилось число ${number}. Количество интераций равно ${counter} `);
 
 
-
+for (let dayNumber = 1; dayNumber <= 31; dayNumber += 7){
+		console.log(`Сегодня пятница, ${dayNumber}-е число. Необходимо подготовить отчет.`)
+}
 
