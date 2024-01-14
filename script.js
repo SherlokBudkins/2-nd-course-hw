ac53049ba1c467694875d821de24dc2fa2f84d08
SherlokBudkins <nurochkater20@mail.ru>
2024-01-14 17:42:54 +0300

    HomeWork6

--- script.js
+++ script.js
@@ -1,115 +1,92 @@
-//Задание 1
-function comparison(a, b) {
-	if (a <= b) {
-		return a;
-	} else {
-		return b;
-	}
+// Задание 1
+const numbs = [1, 5, 4, 10, 0, 3];
+for (let i = 0; i < numbs.length; i++) {
+	if (numbs[i] == 0) break;
+	console.log(numbs[i]);
 }
 
-console.log(comparison(8, 4));
-console.log(comparison(6, 6));
 
-
-//задание 2
-function num(a) {
-	if (a % 2 == 0) {
-        return `Число четное`;
-    } else {
-        return `Число нечетное`;
-    }	
-}
-
-console.log(num(6));
-console.log(num(5));
-
-
+//Задание 2
+const numbs2 = [1, 5, 4, 10, 0, 3];
+console.log(numbs2.indexOf(4));
 
 //Задание 3
-function square(a) {
-    a**2;
-}
+const numbs3 = [1, 3, 5, 10, 20];
+console.log(numbs3.join(' '));
 
-console.log (square(5));
-
-function square(a) {
-    return a**2;
-}
-
-console.log (square(5));
-
-
-//задание 4
-let age = prompt("Сколько Вам лет?");
-
-if (age <= 12) {
-    function printMessage () {
-        console.log("Привет, друг");
-    }
-} else {
-    function printMessage () {
-        console.log("Добро пожаловать!");
-    }
-}
-if (age < 0) {
-
-    function printMessage () {
-        console.log("Вы ввели неправильное значение!");
-}
-}
-
-printMessage();
-
+//Задание 4
+let arr = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1],
+];
+console.log(arr);
 
 //Задание 5
-function check(a , b) {
-    if (isNaN(a) || isNaN(b)) {
-        return(`Одно или оба значения не являются числом`);
-    } else {
-        return a * b;
-    }
-}
-console.log(check(8 , 5));
-console.log(check(8 , `k`));
-
-
-
+let arr2 = [1, 1, 1];
+arr2.push(2, 2, 2);
+console.log(arr2);
 
 //Задание 6
-function num1() {
-    let userNum = prompt('Введи число');
-    let squared = userNum ** 3;
-    isNaN(userNum) ? console.log('Переданный параметр не является числом') : console.log(`${userNum} в кубе равняется ${squared}`);
-}
-num1();
+let arr3 = [9, 8, 7, 'a', 6, 5];
+arr3.sort();
+arr3.pop(String);
+console.log(arr3);
 
-
-
-// Задание 7
-function getArea() {
-    return 3.14 * (this.radius ** 2);
-}
-
-function getPerimeter() {
-    return 2 * 3.14 * this.radius;
+//Задание 7
+let arr4 = [9, 8, 7, 6, 5];
+if (arr4.includes(Number(prompt('Угадай число')))) {
+    console.log('Угадал');
+} else {
+    console.log('Не угадал');
 }
 
-const circle1 = {
-    radius: 5,
+//Задание 8
+let greetings = 'abcdef';
+let arrayGreetings = greetings.split('');
+arrayGreetings.reverse();
+arrayGreetings = arrayGreetings.join('');
+console.log(arrayGreetings);
 
-    getArea: getArea,
-    getPerimeter: getPerimeter,
-};
+//Задание 9
+const arr5 = [
+    [1, 2, 3],
+    [4, 5, 6],
+];
+const flat = arr5.flat();
+console.log(flat);
 
-const circle2 = {
-    radius: 4,
+//Задание 10
+const arr6 = [3, 4, 5, 6, 7];
 
-    getArea: getArea,
-    getPerimeter: getPerimeter,
-};
+for(let i = 0; i < arr6.length; i++) {
+    console.log(arr6[i] + arr6[i + 1]);
+}
 
-console.log(circle1.getArea());
-console.log(circle1.getPerimeter());
-console.log(circle2.getArea());
-console.log(circle2.getPerimeter());
 
+//Задание 11
+const mult = [2, 3, 4, 5, 6];
+const tax = mult.map(el => el**2);
+console.log(tax);
+
+
+//Задание 12
+function arrSum(num) {
+    for (let i = 0; i < num.length; i++) {
+    console.log(num[i].length);
+} 
+} 
+arrSum(['слово', '', 'слог', 'длинное предложение', 'буква']); 
+
+//Задание 13
+function filterPositive(array) {
+    const negative = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            negative.push(array[i])
+        }
+    }
+    return negative
+}
+console.log(filterPositive([-1, 0, 5, -10, 56]));
+console.log(filterPositive([-25, 25, 0, -1000, -2]));
